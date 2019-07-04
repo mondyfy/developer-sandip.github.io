e76176937ba8cc6ea9e17fa5ae2e27b6f7b385c3
Your Name Here <khaptadisandip@gmail.com>
2019-07-05 00:49:03 +0545

    Updates

---
+++ precache-manifest.bc3555d05b621b09d347a819fa79472c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c831207395c148b6316a5c38509d0783",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38f7717e6ce0f15d2939",
+    "url": "/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "38f7717e6ce0f15d2939",
+    "url": "/static/js/main.1acdbe1f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
