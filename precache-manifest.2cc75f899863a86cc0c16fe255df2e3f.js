6b5eb011f1043e96032728c508019e79543eb969
developer-sandip <khaptadisandip@gmail.com>
2019-10-20 19:34:42 +0545

    Updates

---
+++ precache-manifest.2cc75f899863a86cc0c16fe255df2e3f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d53627db99989c5e620e1c6fa5d71308",
+    "url": "/index.html"
+  },
+  {
+    "revision": "710f044c7c25ca082313",
+    "url": "/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "fe60e8fdfee2abf24334",
+    "url": "/static/js/2.c3ef4538.chunk.js"
+  },
+  {
+    "revision": "710f044c7c25ca082313",
+    "url": "/static/js/main.16801ae5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
